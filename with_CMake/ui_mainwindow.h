f84a38933fdbcd93f8680ff516636bcb1c0b3e91
OlgaBasova <31655186+OlgaBasova@users.noreply.github.com>
2018-07-06 19:15:05 +0300

    Update ui_mainwindow.h

--- with_CMake/ui_mainwindow.h
+++ with_CMake/ui_mainwindow.h
@@ -1,11 +1,3 @@
-/********************************************************************************
-** Form generated from reading UI file 'mainwindow.ui'
-**
-** Created by: Qt User Interface Compiler version 5.2.1
-**
-** WARNING! All changes made in this file will be lost when recompiling UI file!
-********************************************************************************/
-
 #ifndef UI_MAINWINDOW_H
 #define UI_MAINWINDOW_H
 
@@ -23,6 +15,7 @@
 #include <QStatusBar>
 #include <QWidget>
 #include <qstring.h>
+#include <QHBoxLayout>
 
 QT_BEGIN_NAMESPACE
 
@@ -31,71 +24,89 @@ class Ui_MainWindow
 public:
 
     QWidget *centralWidget;
-    QPushButton *pushButtonOpenImage;
-    QLabel *label;
+    QPushButton *pushButtonOpenImage;    
+    QPushButton *pushButtonSaveCannyImage;
     QScrollBar *horizontalScrollBar_Thr1;
+    QScrollBar *horizontalScrollBar_Thr2;
     QGraphicsView *graphicsViewImage;
+    QGraphicsView *graphicsViewCannyImage;
     QSpinBox *spinBox_Thr1;
     QSpinBox *spinBox_Thr2;
     QSpinBox *spinBox_apertureSize;
-    QScrollBar *horizontalScrollBar_Thr2;
-    QPushButton *pushButtonSaveCannyImage;
+    QLabel *label;
     QLabel *label_2;
-    QLabel *label_3;
-    QGraphicsView *graphicsViewCannyImage;
+    QLabel *label_3;    
     QStatusBar *statusBar;
+
+    QGridLayout *allLayout;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
         MainWindow->resize(1070, 652);
-        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
         MainWindow->setSizePolicy(sizePolicy);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+
         pushButtonOpenImage = new QPushButton(centralWidget);
         pushButtonOpenImage->setObjectName(QStringLiteral("pushButtonOpenImage"));
-        pushButtonOpenImage->setGeometry(QRect(10, 20, 131, 23));
+
         label = new QLabel(centralWidget);
         label->setObjectName(QStringLiteral("label"));
-        label->setGeometry(QRect(220, 10, 221, 20));
+        spinBox_Thr1 = new QSpinBox(centralWidget);
+        spinBox_Thr1->setObjectName(QStringLiteral("spinBox_Thr1"));
         horizontalScrollBar_Thr1 = new QScrollBar(centralWidget);
-        horizontalScrollBar_Thr1->setObjectName(QStringLiteral("horizontalScrollBar_Thr1"));
-        horizontalScrollBar_Thr1->setGeometry(QRect(514, 10, 321, 16));
         horizontalScrollBar_Thr1->setOrientation(Qt::Horizontal);
+
+        pushButtonSaveCannyImage = new QPushButton(centralWidget);
+        pushButtonSaveCannyImage->setObjectName(QStringLiteral("pushButtonSaveCannyImage"));
+
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        spinBox_Thr2 = new QSpinBox(centralWidget);
+        spinBox_Thr2->setObjectName(QStringLiteral("spinBox_Thr2"));
+        horizontalScrollBar_Thr2 = new QScrollBar(centralWidget);
+        horizontalScrollBar_Thr2->setObjectName(QStringLiteral("horizontalScrollBar_Thr2"));
+        horizontalScrollBar_Thr2->setOrientation(Qt::Horizontal);
+
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        spinBox_apertureSize = new QSpinBox(centralWidget);
+        spinBox_apertureSize->setObjectName(QStringLiteral("spinBox_apertureSize"));
+
         graphicsViewImage = new QGraphicsView(centralWidget);
         graphicsViewImage->setObjectName(QStringLiteral("graphicsViewImage"));
         graphicsViewImage->setEnabled(true);
-        graphicsViewImage->setGeometry(QRect(10, 100, 521, 521));
-        spinBox_Thr1 = new QSpinBox(centralWidget);
-        spinBox_Thr1->setObjectName(QStringLiteral("spinBox_Thr1"));
-        spinBox_Thr1->setGeometry(QRect(454, 10, 51, 22));
-        spinBox_Thr2 = new QSpinBox(centralWidget);
-        spinBox_Thr2->setObjectName(QStringLiteral("spinBox_Thr2"));
-        spinBox_Thr2->setGeometry(QRect(454, 40, 51, 22));
-        spinBox_apertureSize = new QSpinBox(centralWidget);
-        spinBox_apertureSize->setObjectName(QStringLiteral("spinBox_apertureSize"));
-        spinBox_apertureSize->setGeometry(QRect(454, 70, 51, 22));
-        horizontalScrollBar_Thr2 = new QScrollBar(centralWidget);
-        horizontalScrollBar_Thr2->setObjectName(QStringLiteral("horizontalScrollBar_Thr2"));
-        horizontalScrollBar_Thr2->setGeometry(QRect(514, 40, 321, 16));
-        horizontalScrollBar_Thr2->setOrientation(Qt::Horizontal);
-        pushButtonSaveCannyImage = new QPushButton(centralWidget);
-        pushButtonSaveCannyImage->setObjectName(QStringLiteral("pushButtonSaveCannyImage"));
-        pushButtonSaveCannyImage->setGeometry(QRect(10, 60, 131, 23));
-        label_2 = new QLabel(centralWidget);
-        label_2->setObjectName(QStringLiteral("label_2"));
-        label_2->setGeometry(QRect(220, 40, 231, 20));
-        label_3 = new QLabel(centralWidget);
-        label_3->setObjectName(QStringLiteral("label_3"));
-        label_3->setGeometry(QRect(220, 70, 221, 20));
+        graphicsViewImage->setMinimumSize(300,300);
+        graphicsViewImage->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+
         graphicsViewCannyImage = new QGraphicsView(centralWidget);
         graphicsViewCannyImage->setObjectName(QStringLiteral("graphicsViewCannyImage"));
-        graphicsViewCannyImage->setGeometry(QRect(540, 100, 521, 521));
+        graphicsViewCannyImage->setMinimumSize(300,300);
+        graphicsViewCannyImage->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+
+        allLayout = new QGridLayout;
+        allLayout->addWidget(label, 0, 1);
+        allLayout->addWidget(spinBox_Thr1, 0, 2);
+        allLayout->addWidget(horizontalScrollBar_Thr1, 0, 3);
+
+        allLayout->addWidget(pushButtonOpenImage, 1, 0);
+        allLayout->addWidget(label_2, 1, 1);
+        allLayout->addWidget(spinBox_Thr2, 1, 2);
+        allLayout->addWidget(horizontalScrollBar_Thr2, 1, 3);
+
+        allLayout->addWidget(pushButtonSaveCannyImage, 2, 0);
+        allLayout->addWidget(label_3, 2, 1);
+        allLayout->addWidget(spinBox_apertureSize, 2, 2);
+
+        allLayout->addWidget(graphicsViewImage, 3, 0, 2, 2);
+        allLayout->addWidget(graphicsViewCannyImage, 3, 2, 2, 2);
+
+        centralWidget->setLayout(allLayout);
+
+
         MainWindow->setCentralWidget(centralWidget);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
